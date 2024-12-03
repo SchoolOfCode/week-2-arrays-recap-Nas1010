a5e434e5a4f7cb5e824697f0c29d728687a3ef96
Nas1010 <184663878+Nas1010@users.noreply.github.com>
2024-12-03 18:54:59 +0000

    completed upto task2

---
+++ main.js
@@ -0,0 +1,53 @@
+//Task 1 - Declaring an Array
+
+//const favoriteFoods = ["Cheese", "Chocolate", "Olives", "Anchovies"];
+//1. Favorite Songs
+//2. Profit From First Seven Days of the School of Code's Penny Doubling Pyramid Scheme 💰💰💰
+//3. Friends
+
+const favouriteSongs = ["Tiny Dancer", "Running Up That Hill", "Don't Stop Moving", "Tragedy", "Crazy Horses"];
+
+const profitFromFirstSevenDaysOfSoCpennyDoublingPyramid = [1, 2 , 4 , 8 , 16 , 32 , 64];
+
+const nameAndSkill = [
+    {name: "Chris", skill: "Changing lives."},
+    {name: "Lizzie", skill: "Being friendly"},
+    {name: "Ben",  skill: "Code rapping"},
+    {name: "Tao", skill: "Hand raising"},
+    {name: "Liz", skill: "Crochet"},
+    {name: "Patrick", skill: "Call of Duty"},
+    {name: "Tim", skill: "Analogies"},
+    {name: "James", skill: "Friendship"},
+    {name: "Joseph", skill: "Being positive"},
+    {name: "Max", skill: "Papier-mache"}
+];
+
+console.log(nameAndSkill)
+
+//Task 2 - Reading Array Values
+
+//Using bracket notation and the correct index, for each person...
+//Console.log the month in which you were born.
+//Console.log the month your mother was born.
+//Console.log your favorite month.
+
+let options = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+] 
+
+console.log(options[4]);
+console.log(options[3]);
+console.log(opyions[6]);
+
+
